b87c6e96998bbaa5742d333020064db5fd191755
WinterTech13 <139192188+WinterTech13@users.noreply.github.com>
2024-06-16 13:20:06 -0400

    Minor UI improvements

--- mcbackup.bat
+++ mcbackup.bat
@@ -13,11 +13,11 @@ set Hr=%DateTime:~8,2%
 set Min=%DateTime:~10,2%
 
 echo.
-echo What world would you like to back up?
+echo Which world would you like to back up?
 echo.
 dir /a:d /b
 echo.
-set /p world=World Name 
+set /p world=World Name: 
 
 if not exist "%world%" goto error
 
@@ -29,12 +29,12 @@ copy %BackupName%.zip C:\Users\%USERNAME%\Desktop\%BackupName%.zip
 del %BackupName%.zip
 
 cls
-echo %world% backed up! (Check the desktop!)
+echo %world% backed up! Check the Desktop!
 goto ex
 
 :error
 cls
-echo The world you inputted does not exist! :O
+echo The world you entered does not exist!
 goto ex
 
 :ex
